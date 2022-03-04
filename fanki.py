b4922d2f34fb68ece0d3f5a1f212bb1cce719972
Gustavo Korzune Gurgel <gukgurgel@usp.br>
2022-03-04 20:22:38 +0100

    Add layout using object oriented design
    Signed-off-by: Gustavo Korzune Gurgel <gukgurgel@usp.br>

--- fanki.py
+++ fanki.py
@@ -24,15 +24,18 @@ class Program(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.layout = None
+
+        '''
         self.grid = None
-        self.deck = LineBox('Deck', self.update_status)
-        self.sentence = LineBox('Sentence', self.update_status)
-        self.tr_sentence = LineBox('Translated Sentence', self.update_status)
-        self.words = LineBox('Words', self.update_status)
-        self.tr_words = LineBox('Translated Words', self.update_status)
-        self.synonyms = LineBox('Synonyms', self.update_status)
-        self.front = TextBox('Front', self.update_status)
-        self.back = TextBox('Back', self.update_status)
+        self.deck = LineBox('Deck', self)
+        self.sentence = LineBox('Sentence', self)
+        self.tr_sentence = LineBox('Translated Sentence', self)
+        self.words = LineBox('Words', self)
+        self.tr_words = LineBox('Translated Words', self)
+        self.synonyms = LineBox('Synonyms', self)
+        self.front = TextBox('Front', self)
+        self.back = TextBox('Back', self)
         self.back.edit.setFont(QFont('Helvetica', 15, weight=75))
         self.state = {
             'deck': '',
@@ -47,16 +50,25 @@ class Program(QWidget):
         self.preview_btn = None
         self.add_btn = None
         self.transl_btn = None
+        self.change_mode_btn = None
         self.clear_btn = None
         self.initUI()
+        '''
 
+    def initUI(self):
+        self.layout = Layout(self)
+        self.layout.load(self)
+        self.show()
+
+    '''
     def initUI(self):
 
         mid_point = QDesktopWidget().availableGeometry().center()
-        grid_i = 1
+        grid_i = 0
 
         self.grid = QGridLayout()
-
+        
+        grid_i = self.add_change_mode_btn(grid_i)
         grid_i = self.deck.add(grid_i, self.grid)
         grid_i = self.sentence.add(grid_i, self.grid)
         grid_i = self.words.add(grid_i, self.grid)
@@ -85,6 +97,13 @@ class Program(QWidget):
         self.synonyms.edit.clear()
         self.tr_sentence.edit.clear()
         self.tr_words.edit.clear()
+
+    def add_change_mode_btn(self, grid_i):
+        self.change_mode_btn = QPushButton('Change Mode', self)
+        self.change_mode_btn.setFont(QFont('Helvetica', 15))
+        self.grid.addWidget(self.change_mode_btn, grid_i, 0)
+        self.change_mode_btn.clicked.connect(self.button_processor)
+        return grid_i + 1
 
     def add_translation_btn(self, grid_i):
         self.transl_btn = QPushButton('Translate', self)
@@ -168,42 +187,186 @@ class Program(QWidget):
             self.front.edit.clear()
             self.back.edit.clear()
 
+        if sender.text() == 'Change Mode':
+            for i in range(self.grid.count()):
+                print(i)
+                self.grid.itemAt(i).widget().hide()
+                self.layout.change()
+    '''
 
-class TextBox:
-    def __init__(self, name, update_vars):
 
+class Layout(QGridLayout):
+    
+    def __init__(self, program):
+        super().__init__()
+        self.setObjectName('Layout')
+        self.now = Lingvist(self)
+        self.i_end_of_header = 0
+        mid_point = QDesktopWidget().availableGeometry().center()
+        program.setGeometry(mid_point.x(),
+                            0,
+                            int(mid_point.x() / 2),
+                            mid_point.y())
+
+    def load(self, program):
+        grid_i = 0  # 'i' = row of the grid
+        grid_i = self.load_header(grid_i, program)
+        self.i_end_of_header = grid_i
+        grid_i = self.now.load(self, grid_i, program)
+        self.load_footer(grid_i, program)
+        program.setLayout(self)
+
+    def change(self):
+        self.now = self.now.change(self)
+
+    def clean(self):
+        for i in range(self.grid.count()):
+            self.itemAt(i).widget().hide()
+
+    def load_header(self, grid_i, program):
+        grid_i = Button('Change Mode', program).add(grid_i, self)
+        grid_i = LineBox('Deck', program).add(grid_i, self)
+        return grid_i
+
+    def load_footer(self, grid_i, program):
+        self.addWidget(QHLine(), grid_i, 0) 
+        grid_i += 1
+        # add line separating input fields from the note preview
+
+        grid_i = Button('Preview', program).add(grid_i, self)
+        grid_i = TextBox('Front', program).add(grid_i, self)
+        grid_i = TextBox('Back', program).add(grid_i, self)
+        grid_i = Button('Add Note', program).add(grid_i, self)
+
+class LayoutType:
+    def __init__(self, grid):
+        self.grid = grid
+        self.objects_list = []
+
+    def clean(self):
+        pass
+
+
+class Lingvist(LayoutType):
+
+    def __init__(self, grid):
+        super().__init__(grid)
+        self.type = 'Lingvist'
+
+    def change(self, grid):
+        return LingvistAdvanced(grid)
+
+    def load(self, grid, grid_i, program):
+        self.objects_list = [
+                'Deck', 'Sentence', 'Translated Sentence',
+                'Words', 'Translated Words', 'Synonyms',
+                'Translator'
+                ]
+
+        grid_i = LineBox('Sentence', program).add(grid_i, grid)
+        grid_i = LineBox('Words', program).add(grid_i, grid)
+        grid_i = LineBox('Synonyms', program).add(grid_i, grid)
+        grid_i = Button('Translator', program).add(grid_i, grid) 
+        grid_i = LineBox('Translated Sentence', program).add(grid_i, grid)
+        grid_i = LineBox('Translated Words', program).add(grid_i, grid)
+
+        return grid_i
+
+
+class LingvistAdvanced(LayoutType):
+    def __init__(self, grid):
+        super().__init__(grid)
+        self.type = 'LingvistAdvanced'
+
+    def load(self, grid, grid_i, program):
+        pass
+
+    def change(self, grid):
+        pass
+
+
+class Button(QPushButton):
+
+    def __init__(self, name, program):
+        super().__init__(name, program)
+        self.setParent(program)
+        self.setObjectName(name)
+        self.setFont(QFont('Helvetica', 15))
+        self.clicked.connect(self.processor)
+        self.program = program
+
+    def add(self, line, layout):
+        layout.addWidget(self, line, 0)
+        return line + 1
+
+    def processor(self):
+        program = self.program
+        sender = program.sender()
+        if sender != None:
+            layout = program.layout
+            pressed = sender.text()
+            switcher = {
+                    'Change Mode' : self.change_mode(layout, program),
+                    'Translate' : self.translate(layout, program),
+                    'Preview' : self.preview(layout, program),
+                    'Add Note' : self.add_note(layout, program)
+                    }
+
+            switcher.get(pressed)
+
+
+    def change_mode(self, layout, program):
+        layout.now.clean()
+        #layout.change()
+        #layout.now.load(layout, layout.i_end_of_header, program)
+
+    def translate(self, layout, program):
+        pass
+
+    def preview(self, layout, program):
+        pass
+
+    def add_note(self, layout, program):
+        pass
+
+class TextBox(QTextEdit):
+    def __init__(self, name, program):
+
+        super().__init__(parent=program)
         name_html = ('<center><h2>' + name + '</h2></center>')
         self.label = QLabel(name_html)
         self.label.setTextFormat(2)
-        self.edit = QTextEdit()
-        self.edit.textChanged.connect(update_vars)
-        self.edit.setFont(QFont('Helvetica', 15))
+        if name == 'Back':
+            self.setFont(QFont('Helvetica', 15, weight=75))
+        else:    
+            self.setFont(QFont('Helvetica', 15))
 
     def add(self, line, grid):
         grid.addWidget(self.label, line, 0)
-        grid.addWidget(self.edit, line + 1, 0)
+        grid.addWidget(self, line + 1, 0)
         return line + 2
 
 
-class LineBox:
-    def __init__(self, name, update_vars):
+class LineBox(QLineEdit):
+    def __init__(self, name, program):
 
-        name_html = ('<center><h2>' + name + '</h2></center>')
+        super().__init__(parent=program)
+        name_html = ('<center><h3>' + name + '</h3></center>')
         self.label = QLabel(name_html)
         self.label.setTextFormat(2)
-        self.edit = QLineEdit()
-        self.edit.textChanged.connect(update_vars)
-        self.edit.setFont(QFont('Helvetica', 15))
+        self.setObjectName(name)
+        self.setFont(QFont('Helvetica', 15))
 
     def add(self, line, grid):
         grid.addWidget(self.label, line, 0)
-        grid.addWidget(self.edit, line + 1, 0)
+        grid.addWidget(self, line + 1, 0)
         return line + 2
 
 
 def main():
     app = QApplication(sys.argv)
     prog = Program()
+    prog.initUI()
     sys.exit(app.exec_())
 
 
